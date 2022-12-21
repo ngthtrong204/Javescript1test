2ce5e763784c82aee9fdc886aaaeba93ddfc20e2
ngthtrong204 <ngthtrong204@gmail.com>
2022-12-21 11:10:29 +0700

    last commit

--- baitap3.js
+++ baitap3.js
@@ -1,24 +1,29 @@
-console.log("       Bài tập 3");
 
-    //INPUT:
-    //Số tiền mệnh giá USD
+//INPUT:
+//Số tiền mệnh giá USD
 
-    //Quá trình xử lý:
-        //B1: tạo biến "SoTienUSD", "SoTienVND"; tạo hằng số quy đổi "TyGia" = 23000
-        //B2: Nhập giá trị cho biến "SoTienUSD"
-        //B3: Gán biến "SoTienVND" = "SoTienUSD" * "TyGia"
-        //B4: In ra màn hình console "SoTienVND"
+//Quá trình xử lý:
+//B1: tạo biến "SoTienUSD", "SoTienVND"; tạo hằng số quy đổi "TyGia" = 23000
+//B2: Nhập giá trị cho biến "SoTienUSD"
+//B3: Gán biến "SoTienVND" = "SoTienUSD" * "TyGia"
+//B4: In ra màn hình console "SoTienVND"
 
-    //OUTPUT:
-    //Số tiền mệnh giá VND
+//OUTPUT:
+//Số tiền mệnh giá VND
 
 const TyGia = 23500;
-let SoTienVND;
-let SoTienUSD = 200;
-console.log("Số tiền mệnh giá USD: ",SoTienUSD,"USD")
 
-SoTienVND= TyGia*SoTienUSD;
-console.log("Quy đổi ra là VND là: ",SoTienVND,"VND")
 
-console.log("");
+
+function bai3() {
+    let SoTienUSD = document.getElementById('SoTienUSD').value;
+    let SoTienVND = 0;
+    SoTienVND = TyGia * SoTienUSD;
+  document.getElementById('kq3').value =SoTienVND;
+
+}
+
+
+
+
 
--- baitap4.js
+++ baitap4.js
@@ -1,33 +1,31 @@
-console.log("       Bài tập 4");
+//INPUT:
+//Nhập vào chiều dài và chiều rộng của một hình chữ nhật
 
-    //INPUT:
-    //Nhập vào chiều dài và chiều rộng của một hình chữ nhật
+//Quy trình xử lý:
+//B1: tạo các biến "ChieuDai", "ChieuRong", "ChuVi", "DienTich"
+//B2: nhập giá trị chu hai biến "ChieuDai" và "ChieuRong"
+//B3: gán giá trị theo công thức toán học
+//ChuVi = (ChieuDai + ChieuRong)*2
+//DienTich = ChieuDai*ChieuRong
+//B4: in ra màn hình console kết quả của ChuVi và DienTich
 
-    //Quy trình xử lý:
-        //B1: tạo các biến "ChieuDai", "ChieuRong", "ChuVi", "DienTich"
-        //B2: nhập giá trị chu hai biến "ChieuDai" và "ChieuRong"
-        //B3: gán giá trị theo công thức toán học
-            //ChuVi = (ChieuDai + ChieuRong)*2
-            //DienTich = ChieuDai*ChieuRong
-        //B4: in ra màn hình console kết quả của ChuVi và DienTich
-
-    //OUTPUT:
-    //Xuât ra độ lớn diện tích và chu vi của hình chữ nhật đó
-
-let ChuVi;
-let DienTich;
-let ChieuDai = 3;
-let ChieuRong = 4;
-console.log("Hình chữ nhật có chiều dài: ",ChieuDai," và chiều rộng: ",ChieuRong);
-
-ChuVi = (ChieuDai+ChieuRong)*2;
-DienTich = ChieuRong*ChieuDai;
-
-console.log("Chu vi của hình chữ nhật là: ",ChuVi)
-console.log("Diện tích của hình chữ nhật là: ",DienTich)
+//OUTPUT:
+//Xuât ra độ lớn diện tích và chu vi của hình chữ nhật đó
 
 
 
+function bai4() {
+    let ChieuDai = document.getElementById('ChieuDai').value
+    let ChieuRong = document.getElementById('ChieuRong').value
 
-console.log("");
+    let ChuVi;
+    ChuVi = (Number(ChieuDai) + Number(ChieuRong)) * 2;
+    let kq41 = document.getElementById('kq41')
+    kq41.innerHTML = ChuVi;
 
+    let DienTich;
+    DienTich = Number(ChieuDai) * Number(ChieuRong);
+    let kq42 = document.getElementById('kq42')
+    kq42.innerHTML = DienTich;
+
+}
